2699b70ee2892f154e06106faedbeb21ed439145
Deadly0Sin <s.sheykhroody5@gmail.com>
2023-10-29 10:35:30 -0800

    Create train-class.py

---
+++ train-class.py
@@ -0,0 +1,26 @@
+x = 8
+if type(x) == int :
+    print("this val is int")    
+y ="sina" 
+if type(y) == str :
+    print("this val is str")
+z = 0.3
+if type(z) == float :
+    print("this val is float")
+h = 4 
+if type(h) == complex :
+    print("this val is complex")
+j ="game"
+if type(j) == set :
+    print("this val is set")
+A = {"top", "down", "6"}
+if type (A) == dict :
+    print("this val is dict")   
+A = {"top", "down", "6"}
+if type(A) == set :
+    print("this val is set")
+if type (A) == None :
+    print("this val is none")
+B = None
+if type(B) == None :
+    print("yhis val is none")             
